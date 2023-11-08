bffcbcb15abb72b15368a9c919287c3e6d244a8f
Вячеслав Клепалов <slavaklepalov@gmail.com>
2023-11-08 23:06:40 +0500

    Добавил два новых метода для работы с объектами в бд.

--- app/services/base.py
+++ app/services/base.py
@@ -9,9 +9,33 @@ class BaseDAO:
     model = None
 
     @classmethod
-    async def get_all(cls):
+    async def get_by_id(cls, model_id: int):
+        """
+        Метод для получения экземпляра по id.
+        Если экземпляр не получен - возвращает None.
+        """
+        async with async_session_maker() as session:
+            query = select(cls.model.__table__.columns).filter_by(id=model_id)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
+
+    @classmethod
+    async def get_one_or_none(cls, **filter_by):
+        """
+        Метод для получения одного экземпляра по заданым
+        фильтрам, если такой имеется.
+        Если экземпляр по фильтрам получить не удается - то
+        возвращается None.
+        """
+        async with async_session_maker() as session:
+            query = select(cls.model.__table__.columns).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
+
+    @classmethod
+    async def get_all(cls, **filter_by):
         """Метод для получения всех экземпляров."""
         async with async_session_maker() as session:
-            query = select(cls.model.__table__.columns)
+            query = select(cls.model.__table__.columns).filter_by(**filter_by)
             result = await session.execute(query)
             return result.mappings().all()
