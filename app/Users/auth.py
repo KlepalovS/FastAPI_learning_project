0fe1b2117a81279062c66de43822517bca31da23
Вячеслав Клепалов <slavaklepalov@gmail.com>
2023-11-10 22:00:24 +0500

    Создали функции для получения захэшированного пароля и проверки введенного пароля с захэшированным.

---
+++ app/Users/auth.py
@@ -0,0 +1,17 @@
+from passlib.context import CryptContext
+
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+def get_password_hash(password: str) -> str:
+    """Получает захешированную строку с паролем."""
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password) -> bool:
+    """
+    Проверяет соответствие введенного пароля с захэшированным и
+    возвращает True, если значения совпадают, иначе - False.
+    """
+    return pwd_context.verify(plain_password, hashed_password)
