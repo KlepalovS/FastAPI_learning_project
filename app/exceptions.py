5af517f4f3fe8af67507ef3e6d03a43c7b1f2171
Вячеслав Клепалов <slavaklepalov@gmail.com>
2023-11-18 23:51:03 +0500

    Небольшая работа над ошибками: исправлена работа исключений через ООП.

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,35 +1,66 @@
 from fastapi import HTTPException, status
 
-UserAlreadyExistsException = HTTPException(
-    status_code=status.HTTP_409_CONFLICT,
-    detail="Пользователь уже существует!",
-)
 
-IncorrectEmailOrPasswordException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Неверные почта или пароль!",
-)
+class BookingException(HTTPException):
+    """
+    Базовый класс исключений в приложении,
+    наследуемый от класса HTTPException.
+    Задаем значения по умолчанию (500 код без деталей).
+    """
 
-TokenAbsentException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="В куки нет токена!",
-)
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = ""
 
-TokenExpiredException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Время действия токена истекло!",
-)
+    def __init__(self) -> None:
+        super().__init__(status_code=self.status_code, detail=self.detail)
 
-IncorrectTokenFormatException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Неверный формат токена!",
-)
 
-UserNotFoundException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-)
+class UserAlreadyExistsException(BookingException):
+    """Класс исключения, когда пользователь уже существует в БД."""
 
-NotAdminUserException = HTTPException(
-    status_code=status.HTTP_403_FORBIDDEN,
-    detail='Доступ запрещен! Пользователь с не админ!',
-)
+    status_code = status.HTTP_409_CONFLICT
+    detail = "Пользователь уже существует!"
+
+
+class IncorrectEmailOrPasswordException(BookingException):
+    """Класс исключения, когда введены неверные почта или пароль."""
+
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Неверные почта или пароль!"
+
+
+class TokenAbsentException(BookingException):
+    """Класс исключения, когда токена нет в куки."""
+
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "В куки нет токена!"
+
+
+class TokenExpiredException(BookingException):
+    """Класс исключения, когда истекло время действия токена."""
+
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Время действия токена истекло!"
+
+
+class IncorrectTokenFormatException(BookingException):
+    """Класс исключения, когда полученный токен неверного формата."""
+
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Неверный формат токена!"
+
+
+class UserNotFoundException(BookingException):
+    """Класс исключения, когда нет user_id или пользователя."""
+
+    status_code = status.HTTP_401_UNAUTHORIZED
+
+
+class NotAdminUserException(BookingException):
+    """
+    Класс исключения, когда пользователь, не являясь админом,
+    пытается получить доступ к ресурсу, доступному только админу.
+    """
+
+    status_code = status.HTTP_403_FORBIDDEN
+    detail = 'Доступ запрещен! Пользователь с не админ!'
